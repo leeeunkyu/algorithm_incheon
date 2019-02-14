04fe7a6b18696822b4bd5e9b90dd7c381eb07318
leeeunkyu <xdkyu01@gmail.com>
2019-02-14 14:02:07 +0900

    14503

---
+++ eunkyu/algorithm/src/algorithm/Main_1024.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1024 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str [] = br.readLine().split(" ");
+		int n = Integer.parseInt(str[0]);
+		int l = Integer.parseInt(str[1]);
+		
+		
+	}
+}
