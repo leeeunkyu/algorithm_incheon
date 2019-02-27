af1a091d7445c011044ab0dafa94bf5203061c8f
leeeunkyu <xdkyu01@gmail.com>
2019-02-27 11:33:24 +0900

    15661

--- eunkyu/algorithm/src/algorithm/Main_4811.java
+++ eunkyu/algorithm/src/algorithm/Main_4811.java
@@ -3,12 +3,18 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main_4811 {
+	
+	static BigInteger[][] dp;
+	static StringBuilder sb;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+		sb = new StringBuilder();
 		int n;
+		//W 한개짜리 H는 반개짜리
+		
 		while(true) {
 			//약을꺼내서 한개짜리면 바로먹고 
 			n = Integer.parseInt(br.readLine());
@@ -18,30 +24,35 @@ public class Main_4811 {
 				sb.append(n+"\n");
 				continue;
 			}
-			int[] dp = new int[n + 1];
-			dp[1] = 0;
-			dp[2] = 2;
-			for (int i = 3; i <= n; i++) {
-				int sum = 0;
-				int pivot = i / 2;
-				for (int j = 0; pivot - j >= 1; j++) {
-					if((i % 2) != 0) {
-						int val = (dp[pivot - j] + dp[pivot + j + 1]) * 2;	
-						sum += val;
-					} else {
-						int val = (dp[pivot - j] + dp[pivot + j]);
-						if(j != 0) {
-							val *= 2;
-						}
-						sum += (val);					
-					}
-				}
-				if((i % 2) != 0)
-					sum += (i - 2);
-				dp[i] = sum;
-			}
-			sb.append(dp[n]+"\n");
+			//00000
+
+			dp = new BigInteger[n + 1][n + 1];
+		
+			eatMedicine(n, 0);
+			
+			sb.append(dp[n][0]+"\n");
+
 		}
 		System.out.println(sb);
+	}
+	private static BigInteger eatMedicine(int w, int h) {
+		if (w > 0 && dp[w][h] != null) {
+			//한개짜리가 0보다 크고, dp에 이미 값이 있다면
+			return dp[w][h];
+		}
+		if(w == 0) {
+			dp[w][h] = BigInteger.ONE;
+			return BigInteger.ONE;
+		} else {
+			BigInteger sum = BigInteger.ZERO;
+			sum = sum.add(eatMedicine(w - 1, h + 1));
+			//한개짜리 -1 반개짜리 +1
+			if(h > 0) {
+				sum = sum.add(eatMedicine(w, h - 1));
+				//반개짜리만먹기
+			}
+			dp[w][h] = sum;
+			return sum;
+		}
 	}
 }
