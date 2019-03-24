5e8449f45d8b9eaf89fec83214e5872833b8c0eb
leeeunkyu <xdkyu01@gmail.com>
2019-03-24 19:37:30 +0900

    13460

--- eunkyu/algorithm/src/algorithm/Main_13460.java
+++ eunkyu/algorithm/src/algorithm/Main_13460.java
@@ -5,196 +5,183 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Stack;
 
 public class Main_13460 {
-	final static int MAX_COUNT = 10;
+	
 	static int n;
 	static int m;
+	static char[][] arr;
+	static boolean[][][][] visited;
+	static int res;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str[] = br.readLine().split(" ");
-		n = Integer.parseInt(str[0]); //세로
-		m = Integer.parseInt(str[1]); //가로
-		char bord[][] = new char[n][m];
-		RedBall redBall = new RedBall(0, 0, 0);
-		BlueBall blueBall = new BlueBall(0, 0);
+		String[] str = br.readLine().split(" ");
+		n = Integer.parseInt(str[0]);
+		m = Integer.parseInt(str[1]);
+		arr = new char[n][m];
+		visited = new boolean[n][m][n][m];
+		Ball_13460 red = null;
+		Ball_13460 blue = null;
 		for (int i = 0; i < n; i++) {
-			char[] temp = br.readLine().toCharArray();
+			String info = br.readLine();
 			for (int j = 0; j < m; j++) {
-				char val = temp[j];
-				bord[i][j] = val;
-				if (val == 'R') {
-					redBall = new RedBall(j, i, 0); 
-				} else if (val == 'B') {
-					blueBall = new BlueBall(j, i);
+				char val = info.charAt(j);
+				arr[i][j] = val;
+				if(val == 'R') {
+					red = new Ball_13460(i, j, true, 0);
+				}
+				if(val == 'B') {
+					blue = new Ball_13460(i, j, false, 0);
 				}
 			}
 		}
-
 		
-		new Main_13460().bordGame(bord, redBall, blueBall);
-
+		/*for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
+		}*/
+		res = -1;
+		goGame(red, blue);
+		System.out.println(res);
 	}
-	private void bordGame(char[][] bord, RedBall redBall, BlueBall blueBall) {
+
+	private static void goGame(Ball_13460 red, Ball_13460 blue) {
 		int[] dx = {0, 0, 1, -1};
 		int[] dy = {1, -1, 0, 0};
-		int safeLineX = m - 1;
-		int safeLineY = n - 1;
-
-		Queue<RedBall> redBalls = new LinkedList<RedBall>();
-		Queue<BlueBall> blueBalls = new LinkedList<BlueBall>();
-		
-		redBalls.add(redBall);
-		blueBalls.add(blueBall);
-		boolean isGoal = false;
-		int goalCnt = 0;
-		Stack<Integer> s;
-		while(true) {
-			if(redBalls.isEmpty() && blueBalls.isEmpty())
+		Queue<Ball_13460[]> ballList = new LinkedList<Ball_13460[]>();
+		Ball_13460[] balls = new Ball_13460[2];
+		balls[0] = red;
+		balls[1] = blue;
+		ballList.add(balls);
+		visited[red.getY()][red.getX()][blue.getY()][blue.getX()] = true;
+		while(!ballList.isEmpty()) {
+			balls = ballList.poll();
+			int rx = balls[0].getX();
+			int ry = balls[0].getY();
+			int bx = balls[1].getX();
+			int by = balls[1].getY();
+			int rCnt = balls[0].getCnt();
+			int bCnt = balls[1].getCnt();
+			
+			if(rCnt > 10) break;
+			if(arr[ry][rx] == 'O' && arr[by][bx] != 'O') {
+/*				System.out.println("====== ry: "+ry+" rx:"+rx +" rCnt: "+rCnt+"=====");
+*/				res = rCnt;
 				break;
-			if(!redBalls.isEmpty())
-				redBall = redBalls.poll();
+			}
 			
-			int rx = redBall.getX();
-			int ry = redBall.getY();
-			int cnt = redBall.getCnt();
-			if(!blueBalls.isEmpty())
-				blueBall = blueBalls.poll();
-			
-			int bx = blueBall.getX();
-			int by = blueBall.getY();
-			System.out.println("rx: "+rx+" ry:"+ry+" cnt: "+cnt);
-			loop:
-			for (int i = 0; i < 4; i++) {
-				int nextRX = rx + dx[i];
-				int nextRY = ry + dy[i];
-				int nextBX = bx + dx[i];
-				int nextBY = by + dy[i];
-				int rCnt = 0;
-				int bCnt = 0;
-				isGoal = false;
-				goalCnt = -1;
-				while (true) {
-					boolean r = false;
-					boolean b = false;
-					if(nextBX >= 1 && nextBX < safeLineX && nextBY >= 1 && nextBY < safeLineY
-							 && bord[nextBY][nextBX] != '#') {
-						if(bord[nextBY][nextBX] == 'O') {
-							isGoal = false;
-							goalCnt = -1;
-							continue loop;
+			for (int dir = 0; dir < 4; dir++) {
+				int nextRX = rx + dx[dir];
+				int nextRY = ry + dy[dir];
+				int nextBX = bx + dx[dir];
+				int nextBY = by + dy[dir];
+				
+				while(true) {
+					if (arr[nextRY][nextRX] != '#' && arr[nextRY][nextRX] != 'O') {
+						nextRX += dx[dir];
+						nextRY += dy[dir];
+					} else {
+						if(arr[nextRY][nextRX] == '#') {
+							nextRX -= dx[dir];
+							nextRY -= dy[dir];	
 						}
-						nextBX += dx[i];
-						nextBY += dy[i];
-						b = true;
-						++bCnt;
-					}					
-					if(nextRX >= 1 && nextRX < safeLineX && nextRY >= 1 && nextRY < safeLineY
-							 && bord[nextRY][nextRX] != '#') {
-						if(bord[nextRY][nextRX] == 'O') {
-							isGoal = true;
-							if(goalCnt == -1)
-								goalCnt = cnt + 1;
-							//System.out.println(cnt + 1);
-							//return;
+						break;
+					}
+						
+				}
+				
+				while(true) {
+					if(arr[nextBY][nextBX] != '#' && arr[nextBY][nextBX] != 'O') {
+						nextBX += dx[dir];
+						nextBY += dy[dir];
+					} else {
+						if(arr[nextBY][nextBX] == '#') {
+							nextBX -= dx[dir];
+							nextBY -= dy[dir];	
 						}
-						nextRX += dx[i];
-						nextRY += dy[i];
-						r = true;
-						++rCnt;
+						if(arr[nextBY][nextBX] == 'O') {
+							visited[nextRY][nextRX][nextBY][nextBX] = true;
+						}
+						break;
 					}
 					
-					if(nextBX == nextRX && nextBY == nextRY) {
-						if(rCnt > bCnt) {
-							nextRX -= dx[i];
-							nextRY -= dy[i];
+				}
+							
+				if(nextRX == nextBX && nextRY == nextBY) {
+					if(arr[nextRY][nextRX] != 'O') {
+						int redDist = Math.abs(rx - nextRX) + Math.abs(ry - nextRY);
+						int blueDist = Math.abs(bx - nextBX) + Math.abs(by - nextBY);
+						if(redDist > blueDist) {
+							nextRX -= dx[dir];
+							nextRY -= dy[dir];
 						} else {
-							nextBX -= dx[i];
-							nextBY -= dy[i];
-						}
-						break;
-					}
-
-					if(!r && !b) {
-						break;
+							nextBX -= dx[dir];
+							nextBY -= dy[dir];
+						}	
+					} else {
+						visited[nextRY][nextRX][nextBY][nextBX] = true;
 					}
 				}
-				nextBX -= dx[i];
-				nextBY -= dy[i];
-				nextRX -= dx[i];
-				nextRY -= dy[i];
-				blueBalls.add(new BlueBall(nextBX, nextBY));
 				
-				redBalls.add(new RedBall(nextRX, nextRY, cnt + 1));	
-
-			}
-			if(isGoal) {
-				System.out.println(goalCnt);
-				return;
-			}
-			if(cnt == MAX_COUNT) {
-				System.out.println(-1);
-				return;
+				if(!visited[nextRY][nextRX][nextBY][nextBX]) {
+					visited[nextRY][nextRX][nextBY][nextBX] = true;
+				/*	System.out.println("ry: "+ry+" rx:"+rx +" rCnt: "+rCnt);
+					System.out.println("nextRY: "+nextRY+" nextRX:"+nextRX +" rCnt: "+(rCnt + 1));
+*/
+					Ball_13460[] temp = new Ball_13460[2];
+					temp[0] = new Ball_13460(nextRY, nextRX, true, rCnt + 1);
+					temp[1] = new Ball_13460(nextBY, nextBX, false, bCnt + 1);
+					ballList.add(temp);
+				}
 			}
 		}
-		System.out.println(-1);
+		
+		
 	}
 }
 
-class RedBall {
-	int x;
+class Ball_13460 {
 	int y;
+	int x;
+	boolean type;
 	int cnt;
-
-	public RedBall(int x, int y, int cnt) {
+	
+	public Ball_13460(int y, int x, boolean type, int cnt) {
 		super();
-		this.x = x;
 		this.y = y;
+		this.x = x;
+		this.type = type;
 		this.cnt = cnt;
 	}
-
-	public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
+	
 	public int getY() {
 		return y;
 	}
 	public void setY(int y) {
 		this.y = y;
 	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public boolean isType() {
+		return type;
+	}
+	public void setType(boolean type) {
+		this.type = type;
+	}
+
 	public int getCnt() {
 		return cnt;
 	}
+
 	public void setCnt(int cnt) {
 		this.cnt = cnt;
 	}
-}
-
-class BlueBall {
-	int x;
-	int y;
 	
-	public BlueBall(int x, int y) {
-		super();
-		this.x = x;
-		this.y = y;
-	}
-	
-	public int getX() {
-		return x;
-	}
-	public void setX(int x) {
-		this.x = x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
 }
-
--- eunkyu/algorithm/src/algorithm/Main_4902.java
+++ eunkyu/algorithm/src/algorithm/Main_4902.java
@@ -3,6 +3,7 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -16,37 +17,52 @@ public class Main_4902 {
 	static int size;
 	static boolean[][] type;
 	static int width;
+	static int max = Integer.MIN_VALUE;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] str = br.readLine().split(" ");
 		StringBuilder sb = new StringBuilder();
+		int tc = 1;
 		while(Integer.parseInt(str[0]) != 0) {
 			n = Integer.parseInt(str[0]);
 			width = (2 * n) - 1;
 			arr = new int[n][width];
 			type = new boolean[n][width];
-		
+			boolean check = (n % 2 == 1) ? true : false; //È¦¼öÀÌ¸é true Â¦¼öÀÌ¸é false
 			init();
-			
 			int temp = 1;
 			for (int i = 0; i < n; i++) {
 				int width = i * 2 + 1;
 				int k = 0;
 				for (int j = n - (1 + i); k < width; j++) {
 					arr[i][j] = Integer.parseInt(str[temp]);
-					if((i + j) % 2 == 0)
 						type[i][j] = true;
+					if(check && ((i + j) % 2 == 1))
+						type[i][j] = false;
+					else if(!check && ((i + j) % 2 == 0))
+						type[i][j] = false;
 					++temp;
 					++k;
 				}
 			}			
-			sb.append(dfs(0)+"\n");
 			
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < width; j++) {
+					if(type[i][j] && arr[i][j] < 1001) {
+						dfs(i, j, true);
+					}
+					if(!type[i][j] && arr[i][j] < 1001) {
+						dfs(i, j, false);
+					}
+				}
+			}
+			sb.append(tc+". "+max+"\n");
+			++tc;
+			max = Integer.MIN_VALUE;
 			str = br.readLine().split(" ");
 		}
 		System.out.println(sb);
-	
-	
 	}
 
 	private static void init() {
@@ -55,98 +71,77 @@ public class Main_4902 {
 				arr[i][j] = 1001;
 			}
 		}
-		
 	}
 
-	private static int dfs(int cnt) {
-		int res = 0;
-		int max = Integer.MIN_VALUE;
-		for (; cnt < n; cnt++) {
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < width; j++) {
-					if(type[i][j]) { 
-						res = findVal(i, j, cnt + 1);
-						if(res > max) {
-							max = res;
-						}
-					}
-				}
-			}	
+	private static void dfs(int y, int x, boolean type) {
+		if(max < arr[y][x]) {
+			max = arr[y][x];
 		}
-		
-		return max;
+		if(type) {
+			int resSize = n - y;			
+			int[] resList = new int[resSize];
+			resList[0] = arr[y][x];
+			for (int i = 1; i < resSize; i++) {
+				int val = findVal(y + i, x, i);
+				resList[i] = resList[i - 1] + val; 
+				if(max < resList[i]) {
+					max = resList[i];
+				}
+			}
+		} else {
+			int resSize = y + 1;
+			int[] resList = new int[resSize];
+			resList[0] = arr[y][x];
+			for (int i = 1; i < resSize; i++) {
+				int val = findVal2(y - i, x, i);
+				resList[i] = resList[i - 1] + val;
+				if(max < resList[i]) {
+					max = resList[i];
+				}
+			}
+		}
 	}
 
-	private static int findVal(int y, int x, int cnt) {
+	//¿ª »ï°¢Çü
+	private static int findVal2(int y, int x, int step) {
+		int dx[] = setDx(step + 1);
 		int sum = 0;
-		//int sum = arr[y][x];
-		Map<Integer, Integer> resMap = new HashMap<Integer, Integer>();
-		resMap.put(0, 1);	//1Àº Àß µ¹¾Æ°¬´Ù´Â ÀÇ¹Ì
-		
-		Queue<Point_4902> points = new LinkedList<Point_4902>();
-		points.add(new Point_4902(y, x, 1));
-		while(!points.isEmpty()) {
-			Point_4902 point = points.poll();
-			int px = point.getX();
-			int py = point.getY();
-			int size = point.getSize();
-			if(size >= cnt) {
-				return sum;
+		for (int i = 0; i < (step + 1) * 2 - 1; i++) {
+			int nextX = x + dx[i];//1(2Ãþ),2(3Ãþ)...
+			if(nextX >= 0 && nextX < width) {
+				if(arr[y][nextX] >= 1001) {
+					sum = -999999;
+					break;
+				}
+				sum += arr[y][nextX];	
+			} else {
+				sum = -999999;
+				break;
 			}
 			
-			int[] dx = setDx(cnt);
-			int[] dy = setDy(cnt);
-			
-			System.out.println(cnt);
-			for (int i = 0; i < cnt * cnt; i++) {
-				int nextX = px + dx[i];
-				int nextY = py + dy[i];
-				
-				if(nextX >= 0 && nextX < width && nextY >= 0 && nextY < n
-						&& arr[nextY][nextX] < 1000) {
-					sum += arr[nextY][nextX];
-					points.add(new Point_4902(nextY, nextX, size + 1));
-				} else {
-					return Integer.MIN_VALUE;
-				}
-			}	
 		}
 		return sum;
 	}
 
-	private static int[] setDy(int size) {
-		int[] dy = new int[size * size];
-		int temp = 0;
-		int split = 1;
-		int cnt = 0;
-		for (int i = 0; i < size * size; i++) {
-			if(split == cnt) {
-				split += 2;
-				cnt = 0;
-				temp += 1;
-			}
-			dy[i] = temp;
-			cnt += 1;
-			
+	//Á¤ »ï°¢Çü
+	private static int findVal(int y, int x, int step) {
+		int dx[] = setDx(step + 1);
+		int sum = 0;
+		for (int i = 0; i < (step + 1) * 2 - 1; i++) {
+			int nextX = x + dx[i];//1(2Ãþ),2(3Ãþ)...
+			sum += arr[y][nextX];
 		}
-		return dy;
+		return sum;
 	}
 
 	private static int[] setDx(int size) {
-		int dx[] = new int[size * size];
-		dx[0] = 0;
-		int temp = 0;
-		int split = 1;
-		int cnt = 1;
-		for (int i = 0; i < size * size; i++) {
-			if(split <= cnt) {
-				temp = split * -1;
-				cnt = 0;
-				split += 1;
-			}
+		int xSize = size * 2 - 1;//2ÃþÀº 3°³ 3ÃþÀº 5°³...
+		int dx[] = new int[xSize];
+		int temp = (size * -1) + 1;
+		//2Ãþ(size = 2) -1,0,1    3Ãþ(size = 3) -2, -1, 0, 1, 2
+		for (int i = 0; i < xSize; i++) {
 			dx[i] = temp;
-			temp ++;
-			cnt++;
+			temp++;
 		}
 		return dx;
 	}
